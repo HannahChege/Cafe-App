42138735bfa09a5f89039497260f42ebabcceccd
Hannah <chegehannah45>
2019-06-07 07:06:21 +0300

    Changes made on the menu.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Toolbar from './components/Toolbar/Toolbar';
+import  Menus from  './components/Menus';
+import  AddFoodItems from  './components/AddFoodItems';
 import './App.css';
 
 class App extends Component {
---
+++ src/components/Menus.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+document.write( '<div class=\"landingpage\">\n' );
+document.write( '    <img src=\"img/logoBig.png\" class=\"img\" alt=\"logoBig\">\n' );
+document.write( '    <h1 style=\"color:white;\">We provide the best food</h1>\n' );
+document.write( '    <h1 style=\"color:white;\">At the right price</h1>\n' );
+document.write( '  </div>' );
